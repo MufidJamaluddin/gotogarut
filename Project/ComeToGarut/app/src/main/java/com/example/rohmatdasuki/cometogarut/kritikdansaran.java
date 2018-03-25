10acb406ea06f2b0f458ab7e23b80d31707667ce
Rohmat Dasuki <dasukirohmat@gmail.com>
2018-03-25 18:14:44 +0700

    implementasi mockup
    sudah beres semua

---
+++ Project/ComeToGarut/app/src/main/java/com/example/rohmatdasuki/cometogarut/kritikdansaran.java
@@ -0,0 +1,13 @@
+package com.example.rohmatdasuki.cometogarut;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kritikdansaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kritikdansaran);
+    }
+}
