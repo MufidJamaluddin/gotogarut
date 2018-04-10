ab8e472400100fe0a517127373f9029477c8f862
MufidJamaluddin <mufid.jamaluddin@gmail.com>
2018-04-10 19:42:59 +0700

    Merapikan Tampilan

--- Project/ComeToGarut/app/src/main/java/com/example/rohmatdasuki/cometogarut/kritikdansaran.java
+++ Project/ComeToGarut/app/src/main/java/com/example/rohmatdasuki/cometogarut/kritikdansaran.java
@@ -3,10 +3,12 @@ package com.example.rohmatdasuki.cometogarut;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class kritikdansaran extends AppCompatActivity {
+public class kritikdansaran extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kritikdansaran);
     }
